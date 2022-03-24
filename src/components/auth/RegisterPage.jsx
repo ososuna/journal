45e4899b28659999fa85de4a49e8eb340953a774
oswi <oswaldo.adrian@live.com.mx>
2022-03-24 14:58:48 -0600

    register form

--- src/components/auth/RegisterPage.jsx
+++ src/components/auth/RegisterPage.jsx
@@ -1,16 +1,36 @@
 import { Link } from 'react-router-dom';
+import { useForm } from '../../hooks/useForm';
 
 export const RegisterPage = () => {
+  
+  const [ formValues, handleInputChange ] = useForm({
+    name: 'rengoku',
+    email: 'rengoku@test.com',
+    password: '123456',
+    password2: '123456'
+  });
+  
+  const { name, email, password, password2 } = formValues;
+
+  const handleRegister = ( e ) => {
+    e.preventDefault();
+    console.log({
+      name, email, password, password2
+    });
+  }
+  
   return (
     <>
       <h3 className="auth__title">register</h3>
-      <form>
+      <form onSubmit={ handleRegister }>
         <input
           type="text"
           placeholder="name"
           name="name"
           className="auth__input"
           autoComplete="off"
+          value={ name }
+          onChange={ handleInputChange }
         />
         <input
           type="email"
@@ -18,23 +38,28 @@ export const RegisterPage = () => {
           name="email"
           className="auth__input"
           autoComplete="off"
+          value={ email }
+          onChange={ handleInputChange }
         />
         <input
           type="password"
           placeholder="password"
           name="password"
           className="auth__input"
+          value={ password }
+          onChange={ handleInputChange }
         />
         <input
           type="password"
           placeholder="confirm password"
           name="password2"
           className="auth__input"
+          value={ password2 }
+          onChange={ handleInputChange }
         />
         <button
           type="submit"
           className="btn btn-primary btn-block mb-5"
-          disabled={ true } 
         >
           register
         </button>
