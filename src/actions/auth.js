d089a08f556dc7582e0303fc7f0456e47a1defd9
oswi <oswaldo.adrian@live.com.mx>
2022-04-01 17:17:06 -0600

    login w user and password

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -3,9 +3,13 @@ import { types } from '../types/types';
 
 export const startLoginEmailPassword = ( email, password ) => {
   return ( dispatch ) => {
-    setTimeout(() => {
-      dispatch( login(123, 'zenitsu') );
-    }, 3000);
+    firebase.auth().signInWithEmailAndPassword( email, password )
+      .then( ({ user }) => {
+        dispatch( login( user.uid, user.displayName ) );
+      })
+      .catch( e => {
+        console.log( e );
+      } );
   }
 }
 
