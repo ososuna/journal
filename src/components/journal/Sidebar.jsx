bf21840d1c29bc2e557d4e5b192d074a63b6237b
oswi <oswaldo.adrian@live.com.mx>
2022-04-03 17:51:31 -0500

    logout

--- src/components/journal/Sidebar.jsx
+++ src/components/journal/Sidebar.jsx
@@ -1,6 +1,15 @@
-import { JournalEntries } from "./JournalEntries"
+import { useDispatch } from 'react-redux';
+import { startLogout } from '../../actions/auth';
+import { JournalEntries } from './JournalEntries';
 
 export const Sidebar = () => {
+  
+  const dispatch = useDispatch();
+
+  const handleLogout = () => { 
+    dispatch( startLogout() );
+  }
+  
   return (
     <aside className="journal__sidebar">
       <div className="journal__sidebar-navbar">
@@ -8,7 +17,10 @@ export const Sidebar = () => {
           <i className="fa fa-fire"></i>
           <span> rengoku</span>
         </h3>
-        <button className="btn">
+        <button
+          className="btn"
+          onClick={ handleLogout }
+        >
           logout
         </button>
       </div>
